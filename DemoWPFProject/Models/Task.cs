e21ff88ff14cf8a973654ac3ca513b706c0b16d6
Anatoly Kusov <kusov.anatoly@gmail.com>
2025-01-15 17:57:25 +0300

    Add project files.

---
+++ DemoWPFProject/Models/Task.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoWPFProject.Models;
+
+public class Task
+{
+    public string Title { get; set; } = null!;
+    public DateTime DueDate { get; set; }
+    public bool IsCompleted { get; set; }
+}
---
+++ DemoWPFProject/ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace DemoWPFProject.ViewModels;
+
+public partial class MainViewModel: ObservableObject
+{
+    [ObservableProperty]
+    private string _title = null!;
+
+    [ObservableProperty]
+    private Models.Task _selectedTask = null!;
+
+    [ObservableProperty]
+    private ObservableCollection<Models.Task> _tasks;
+
+    public MainViewModel()
+    {
+        Tasks = new ObservableCollection<Models.Task>();
+    }
+
+    [RelayCommand]
+    private void Add()
+    {
+        var task = new Models.Task
+        {
+            Title = Title,
+            DueDate = DateTime.Now,
+            IsCompleted = false,
+        };
+
+        Tasks.Add(task);
+    }
+
+    [RelayCommand]
+    private void Remove()
+    {
+        if (SelectedTask is not null)
+        {
+            Tasks.Remove(SelectedTask);
+        }
+    }
+
+    [RelayCommand]
+    private void SetCompleted()
+    {
+        SelectedTask.IsCompleted = true;
+    }
+}
